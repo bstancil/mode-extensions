4bc9126024b599e4cbcaafdebb6cbc4ba29a75a9
bstancil <bennstancil@gmail.com>
2015-12-23 13:50:06 -0800

    testing 2

--- mode.js
+++ mode.js
@@ -1,1 +1,3 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js">
+var imported = document.createElement("script");
+imported.src = "https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js";
+document.head.appendChild(imported);
